e64c610c16cd7d85dc96566ce37b04c925e2fd34
PickleYard <pickleyard@gmail.com>
2024-04-29 22:51:02 -0700

    Big Update, turning off Stripe payments and switching to ETH/USDC only for now.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,10 @@
 "use client";
+require("dotenv").config();
 
 import "@thirdweb-dev/react";
-import { Elements } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
-import {
-  PaymentElement,
-  useElements,
-  useStripe,
-} from "@stripe/react-stripe-js";
-
 import {
   ConnectWallet,
   useAddress,
-  useContract,
-  useContractMetadata,
   MediaRenderer,
   ThirdwebProvider,
   metamaskWallet,
@@ -21,77 +12,84 @@ import {
   walletConnect,
   trustWallet,
   phantomWallet,
-  Web3Button,
-  useSDKChainId,
-
+  useSigner,
+  localWallet,
+  embeddedWallet,
+  ThirdwebSDK
 } from "@thirdweb-dev/react";
 import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
-
-import { ThirdwebSDK } from "@thirdweb-dev/sdk";
-
 import { 
   NEXT_PUBLIC_THIRDWEB_CLIENT_ID,
-  NEXT_PUBLIC_NFT_CONTRACT_ADDRESS,
-  NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
-  BACKEND_WALLET_ADDRESS,
-  NEXT_PUBLIC_CLIENT_ID_2
-} from "../app/consts/addresses"
+  RECIPIENT_ADDRESS,
+} from "./consts/addresses"
 
 import {
   createThirdwebClient,
   prepareTransaction,
   toWei,
   sendAndConfirmTransaction,
-  waitForReceipt,
-  sendTransaction,
+  prepareContractCall,
+  getContract,
+  sendTransaction
 } from "thirdweb";
-import { polygonMumbai } from "thirdweb/chains";
-
+import { sepolia } from "thirdweb/chains";
+import { ethers5Adapter } from "thirdweb/adapters/ethers5";
+import { ThirdwebProvider as ThirdwebProviderV5 } from "thirdweb/react"
 
 const client = createThirdwebClient({
-  secretKey: NEXT_PUBLIC_THIRDWEB_CLIENT_ID,
+  clientId: NEXT_PUBLIC_THIRDWEB_CLIENT_ID,
 });
-
-// instantiate the SDK with a simple chain name or chain id
-const sdk = new ThirdwebSDK("mumbai");
 
 export default function Home() {
   return (
     <ThirdwebProvider
-      activeChain="mumbai"
+      activeChain="sepolia"
       clientId="c13572bca94a4faabca8160f9b7cae43"
       supportedWallets={[
         metamaskWallet({ recommended: true }),
         coinbaseWallet({ recommended: true }),
+        trustWallet(),
+        phantomWallet(),
         walletConnect(),
-        trustWallet({ recommended: true }),
-        phantomWallet({ recommended: true }),
+        embeddedWallet()
       ]}
     >
-      <PurchasePage />
+      <ThirdwebProviderV5>
+        <PurchasePage />
+      </ThirdwebProviderV5>
     </ThirdwebProvider>
   );
 }
 
-
 function PurchasePage() {
   const address = useAddress();
-  const { contract } = useContract(NEXT_PUBLIC_NFT_CONTRACT_ADDRESS);
+  const signer = useSigner();
   const [isLoading, setIsLoading] = useState(false);
-  // const { contract } = useContract(
-  //  NEXT_PUBLIC_CONTRACT_ADDRESS,
-  //);
   const [ethPaymentMessage, setEthPaymentMessage] = useState('');
+  const [usdcPaymentMessage, setUsdcPaymentMessage] = useState('');
+  const [paymentMethod, setPaymentMethod] = useState("");
+
+  useEffect(() => {
+    if (!signer) {
+      console.log("Waiting for signer...");
+    } else {
+      console.log("Signer is available");
+      // You can now use the signer for transactions
+    }
+  }, [signer]);
 
   const sendEth = async () => {
-    if (!address) {
-      console.error("Wallet is not connected");
+  // Make sure the signer is available
+    if (!address || !signer) {
+      alert("Please connect your wallet.");
       return;
     }
 
     setIsLoading(true);
+    setPaymentMethod("ETH");
     try {
+
       // Fetch the current ETH price in USD
       const response = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd");
       const data = await response.json();
@@ -100,26 +98,29 @@ function PurchasePage() {
       // Calculate the amount of ETH for $98 USD
       const usdAmount = 98;
       const amountInEth = usdAmount / ethPriceInUsd;
-      const amountInWei = ethers.utils.parseUnits(amountInEth.toString(), "ether");
 
-      // Make sure the signer is available
-      const signer = sdk.getSigner();
-      if (!signer) {
-        alert("Signer is not available. Make sure your wallet is connected.");
-        setIsLoading(false);
-        return;
-      }
-
-      // Send ETH transaction
-      const txResponse = prepareTransaction({
-        to: NEXT_PUBLIC_NFT_CONTRACT_ADDRESS,
-        value: toWei(amountInWei),
-        chain: "mumbai",
+      // Prepare ETH transaction
+      const transaction = prepareTransaction({
+        to: RECIPIENT_ADDRESS,
+        value: toWei('1'), // after Demo --> value: toWei(amountInEth.toString()), 
+        chain: sepolia,
         client: client,
       });
 
-      await txResponse.wait(); // Wait for the transaction to be mined
-      setEthPaymentMessage("ETH payment successful!");
+
+      // convert the signer to used with the new SDK
+      const account = await ethers5Adapter.signer.fromEthers(signer);
+      // then use the new SDK normally
+
+      const txReceipt = await sendAndConfirmTransaction({
+        transaction,
+        account,
+      });
+      // Accessing the transaction hash directly
+      const txHash = txReceipt.transactionHash;
+      console.log(txHash);
+
+      setEthPaymentMessage(txHash);
     } catch (error) {
       console.error("Payment error:", error);
       setEthPaymentMessage("ETH payment failed.");
@@ -127,30 +128,69 @@ function PurchasePage() {
       setIsLoading(false);
     }
   };
-  
 
-//----------------------------------------------------------------
-  const { data: contractMetadata } = useContractMetadata(contract);
-  const [clientSecret, setClientSecret] = useState("");
+  const sendUsdc = async () => {
+    // Make sure the signer is available
+    if (!address || !signer) {
+      alert("Please connect your wallet.");
+      return;
+    }
 
-  const onClickStripePayment = async () => {
-    const resp = await fetch("/api/stripe-intent", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        buyerWalletAddress: address,
-      }),
+    setIsLoading(true);
+    setPaymentMethod("USDC");
+
+    try {
+    // Fetch the current USDC contract address
+    const contract = getContract({
+      client,
+      chain: sepolia,
+      address: "0x94a9D9AC8a22534E3FaCa9F4e7F2E2cf85d5E4C8",
     });
-    if (resp.ok) {
-      const json = await resp.json();
-      setClientSecret(json.clientSecret);
+
+
+    // Specify the amount of USDC to send (in smallest unit, e.g., wei for ETH)
+    const amount = ethers.utils.parseUnits("98.0", 6); // Setting USDC amount (6 decimal places)
+    // console.log(amount);
+    const usdcAmount = BigInt(amount.toString()); // Convert BigNumber to bigint
+    // console.log(usdcAmount);
+
+    
+    // convert the signer to used with the new SDK
+    const account = await ethers5Adapter.signer.fromEthers(signer);
+
+    // Prepare USDC transaction
+    const transaction = prepareContractCall({
+      contract,
+      method: {
+        name: "transfer",
+        type: "function",
+        inputs: [
+          { name: "to", type: "address" },
+          { name: "value", type: "uint256" },
+        ],
+        outputs: [],
+        stateMutability: "payable"
+        },
+      params: [RECIPIENT_ADDRESS, usdcAmount],
+    });
+
+
+    // Use the new SDK normally
+    const txReceipt = await sendAndConfirmTransaction({
+      transaction,
+      account,
+    });
+      // Accessing the transaction hash directly
+      const txHash = txReceipt.transactionHash;
+
+      setUsdcPaymentMessage(txHash);
+    } catch (error) {
+      console.error("Payment error:", error);
+      setUsdcPaymentMessage("USDC payment failed.");
+    } finally {
+      setIsLoading(false);
     }
   };
-
-  if (!NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
-    throw 'Did you forget to add a ".env.local" file?';
-  }
-  const stripe = loadStripe(NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
 
   return (
     <main className="flex flex-col gap-y-8 items-center p-12">
@@ -159,23 +199,15 @@ function PurchasePage() {
         btnTitle={"Connect"}
         modalTitle={"Cultivate Crypto Citadel 🪙"}
         switchToActiveChain={true}
-        modalSize={"compact"}
-        welcomeScreen={{
-          img: {
-            src: "https://cdn.discordapp.com/attachments/1096805918114586694/1220244909559382067/Cultivate_Crypto.jpg?ex=660e3ca1&is=65fbc7a1&hm=bd4b540cd90c8651fa78a84954634fc20df50e85a339524c897506bf9e85c3df&",
-            width: 150,
-            height: 150,
-          },
-        }}
+        modalSize={"wide"}
         modalTitleIconUrl={
-          "https://cdn.discordapp.com/attachments/1096805918114586694/1220244909559382067/Cultivate_Crypto.jpg?ex=660e3ca1&is=65fbc7a1&hm=bd4b540cd90c8651fa78a84954634fc20df50e85a339524c897506bf9e85c3df&"
+          "https://cdn.discordapp.com/attachments/1096805918114586694/1220244909559382067/Cultivate_Crypto.jpg?ex=662fdae1&is=662e8961&hm=bd0d6c9e6a286e1fae0031fdabf6e7cf43dc0dca3a3aff7ebaad3f9de318410b&"
         }
         termsOfServiceUrl={""}
         privacyPolicyUrl={""}
         showThirdwebBranding={false}
       />
 
-      {contractMetadata && (
         <div className="flex flex-col gap-8 border border-gray-700 rounded-xl p-12">
           <MediaRenderer
             className="rounded-lg"
@@ -189,112 +221,42 @@ function PurchasePage() {
               marginLeft: 'auto', 
               marginRight: 'auto'
             }}
-            src="https://cdn.discordapp.com/attachments/1096805918114586694/1220244909559382067/Cultivate_Crypto.jpg?ex=660e3ca1&is=65fbc7a1&hm=bd4b540cd90c8651fa78a84954634fc20df50e85a339524c897506bf9e85c3df&"
-//          ⬆️Use this for an NFT drop instead:(src={contractMetadata.image || "Your IPFS link to your NFT collection"})
-//        ⬇️Use (<h2 className="text-xl font-extrabold">{contractMetadata.name}</h2>) instead for NFT Drop Contract⬇️
-          />
+            src="https://cdn.discordapp.com/attachments/1096805918114586694/1220244909559382067/Cultivate_Crypto.jpg?ex=662fdae1&is=662e8961&hm=bd0d6c9e6a286e1fae0031fdabf6e7cf43dc0dca3a3aff7ebaad3f9de318410b&"/>
 
-          
           <div className="flex flex-col gap-2">
             <h2 className="text-xl font-extrabold" style={{ textAlign: "center", margin: "0"}}>{"Join the Citadel"}
             </h2>
-            <p className="text-gray-500">
-              {contractMetadata.description || 
-              "Pay w/ Crypto or Card.💳 Faster Verification!✅"}
+            <p className="text-gray-500" style={{ textAlign: "center", margin: "0"}}>
+              Now you can Subscribe with Crypto!!! ✅
             </p>
           </div>
 
-          {!clientSecret ? (
             <><button 
               className="bg-blue-600 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-lg disabled:bg-gray-400 disabled:opacity-50"
               onClick={sendEth}
               disabled={!address}
               >
-                Send ETH
-              </button>  {/*<-- Button to send eth*/}
-              {isLoading ? 'Processing...' : ''}
-            <button
-              className="bg-blue-600 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-lg disabled:bg-gray-400 disabled:opacity-50"
-              onClick={onClickStripePayment}
-              disabled={!address}
-            >
-                Pay with credit card
+                Subscribe w/ Eth
               </button></>
-          ) : (
-            <Elements
-              options={{
-                clientSecret,
-                appearance: { theme: "night" },
-              }}
-              stripe={stripe}
-            >
-              <CreditCardForm />
-            </Elements>
-          )}
+              <><button 
+              className="bg-blue-600 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-lg disabled:bg-gray-400 disabled:opacity-50"
+              onClick={sendUsdc}
+              disabled={!address}
+              >
+                Subscribe w/ USDC
+              </button>
+              {isLoading ? 'Processing...' : ''}</>
         </div>
-        
-      )}
-      {ethPaymentMessage && (
-  <p className="text-white mt-2">{ethPaymentMessage}</p>
-)}
+        <p className="text-white mt-2">
+  {paymentMethod === "ETH" && ethPaymentMessage ? (
+    <span>ETH payment successful! Transaction: <a href={`https://sepolia.etherscan.io/tx/${ethPaymentMessage}`} target="_blank" style={{ color: '#4CAF50' }}>{ethPaymentMessage}</a></span>
+  ) : null}
+  {paymentMethod === "USDC" && usdcPaymentMessage ? (
+    <span>USDC payment successful! Transaction: <a href={`https://sepolia.etherscan.io/tx/${usdcPaymentMessage}`} target="_blank" style={{ color: '#0096c7' }}>{usdcPaymentMessage}</a></span>
+  ) : null}
+</p>
     </main>
   );
 }
 
-const CreditCardForm = () => {
-  const elements = useElements();
-  const stripe = useStripe();
-  const [isLoading, setIsLoading] = useState(false);
-  const [isCompleted, setIsCompleted] = useState(false);
-  const [ message, setMessage ] = useState("");
 
-  const onClick = async () => {
-    if (!stripe || !elements) {
-      return;
-    }
-
-    setIsLoading(true);
-
-    try {
-      const { paymentIntent, error } = await stripe.confirmPayment({
-        elements,
-        confirmParams: {
-          return_url: "http://localhost:3001",
-        },
-        redirect: "if_required",
-      });
-      if (error) {
-        throw error.message;
-      }
-      if (paymentIntent.status === "succeeded") {
-        setMessage("Payment success. You will recieve your one-time invite shortly.");
-        setIsCompleted(true);
-      } else {
-        alert("Payment failed. Please try again.");
-      }
-    } catch (e) {
-      alert(`There was an error with the payment. ${e}. Please try again.`);
-    }
-
-    setIsLoading(false);
-  };
-
-  return (
-    <><>
-      <PaymentElement />
-
-      <button
-        className="bg-blue-600 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-lg w-full"
-        onClick={onClick}
-        disabled={isLoading || isCompleted || !stripe || !elements}
-      >
-        {isCompleted
-          ? "Payment received"
-          : isLoading
-            ? "Please wait..."
-            : "Pay now"}
-      </button>
-    </><p>{message}</p></>
-  );
-
-};
